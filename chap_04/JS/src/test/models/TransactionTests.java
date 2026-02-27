da112d0c249a253b6fcb2c4687bdcb559c128390
joakor89 <joacorf33@gmail.com>
2026-02-27 11:11:25 -0400

    models: transactions test

---
+++ chap_04/JS/src/test/models/TransactionTests.java
@@ -0,0 +1,25 @@
+package com.blockchainvision.basicblockchain.models;
+
+import com.blockchainvision.basicblockchain.utils.SHA3Helper;
+import org.junit.Test;
+
+/**
+ * (c) Tobias Fertig, FHWS 2018
+ */
+public class TransactionTests
+{
+	@Test
+	public void test()
+	{
+		Transaction t = new Transaction( "sender", "receiver", 1.5, 0, 0.0000005, 0.000001 );
+		Transaction t1 = new Transaction( "fdjkngliuabglbaubf", "fahgibgafadbvbahvbab", 1.5, 0, 0.0000005,
+			0.000001 );
+		Transaction t2 = new Transaction( "sender", "aslkjdfhalbu fviueabuhbiuvbfvbav", 1.5, 0, 0.0000005,
+			0.000001 );
+		System.out.println( t.getTxId( ) );
+		System.out.println( t1.getTxId( ) );
+		System.out.println( SHA3Helper.digestToHex( t2.getTxId( ) ) );
+
+		System.out.println( SHA3Helper.hash256( t ).length );
+	}
+}
