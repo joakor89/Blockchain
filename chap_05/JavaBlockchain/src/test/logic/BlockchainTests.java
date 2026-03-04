86b9e55cefa4b3bc8a090f89d472469d1a58983e
joakor89 <joacorf33@gmail.com>
2026-03-04 14:48:17 -0400

    Test: blockchain test

---
+++ chap_05/JavaBlockchain/src/test/logic/BlockchainTests.java
@@ -0,0 +1,23 @@
+package com.blockchainvision.basicblockchain.logic;
+
+import org.junit.Test;
+
+/**
+ * (c) Tobias Fertig, FHWS 2018
+ */
+public class BlockchainTests
+{
+	@Test
+	public void testFulfillsDifficulty_LessThanDifficulty_TrueReturned( )
+	{
+		Blockchain blockchain = new Blockchain( );
+
+		System.out.println( 0x4000 );
+	}
+
+	@Test
+	public void testFulffilsDifficulty_GreaterThanDifficulty_FalseReturned( )
+	{
+
+	}
+}
