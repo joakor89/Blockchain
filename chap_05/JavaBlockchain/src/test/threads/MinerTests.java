523d6fad8501716da8ca1d52133b4a8a8e907b92
joakor89 <joacorf33@gmail.com>
2026-03-04 14:53:52 -0400

    Miner tests

---
+++ chap_05/JavaBlockchain/src/test/threads/MinerTests.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) Tobias Fertig, 2018.
+ */
+
+package com.blockchainvision.basicblockchain.threads;
+
+import com.blockchainvision.basicblockchain.logic.DependencyManager;
+import com.blockchainvision.basicblockchain.logic.PendingTransactions;
+import com.blockchainvision.basicblockchain.models.Block;
+import com.blockchainvision.basicblockchain.models.Transaction;
+import com.blockchainvision.basicblockchain.utils.SHA3Helper;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MinerTests implements MinerListener
+{
+	@Before
+	public void setUp()
+	{
+		PendingTransactions transactions = DependencyManager.getPendingTransactions( );
+
+		for(int i = 0; i < 100; i++)
+		{
+			String sender = "testSender" + i;
+			String receiver = "testReceiver" + i;
+			double amount = i * 1.1;
+			int nonce = i;
+			double transactionFee = 0.0000001 * i;
+			double transactionFeeLimit = 10.0;
+
+			transactions.addPendingTransaction(
+				new Transaction( sender.getBytes( ),
+					receiver.getBytes( ),
+					amount,
+					nonce,
+					transactionFee,
+					transactionFeeLimit ) );
+		}
+	}
+
+	@Test
+	public void testMiner()
+	{
+		Miner miner = new Miner( );
+		miner.registerListener( this );
+
+		Thread thread = new Thread( miner );
+		thread.start( );
+
+		while ( DependencyManager.getPendingTransactions( ).pendingTransactionsAvailable( ) )
+		{
+			try
+			{
+				Thread.sleep( 1000 );
+			}
+			catch ( InterruptedException e )
+			{
+				e.printStackTrace( );
+			}
+		}
+
+		miner.stopMining( );
+
+		System.out.println( "Länge der Blockchain: " + DependencyManager.getBlockchain( ).size( ) );
+	}
+
+	@Override public void notifyNewBlock( Block block )
+	{
+		System.out.println( "new block mined" );
+
+		System.out.println( SHA3Helper.digestToHex( block.getBlockHash( ) ) );
+	}
+}
